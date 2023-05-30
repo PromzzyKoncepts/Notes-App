912a765bf4e78779b579190310e3becb5c0cffe8
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-05-30 17:18:04 +0100

    implement adding and removing actions

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 // const fs = require('fs');
 // fs.writeFileSync('notes-app.txt', 'this is overlapping the former text')
 // fs.appendFileSync('notes-app.txt', 'this is a message to append to the file')
-// const notes = require('./notes.js');
 // const yargs = require('yargs');
 // const chalk = require('chalk');
-import yargs from "yargs"
-import chalk from "chalk"
 // console.log(process.argv)
 // console.log(yargs.argv)
+
+
+// const notes = require('./notes.js');
+import { addNote, removeNote } from "./notes.js"
+import yargs from "yargs"
+import chalk from "chalk"
 
 // create add command
 yargs.command({
   command: 'add',
   describe: 'Add new note',
-  handler: function () {
-    console.log("adding a new note")
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Body of the note',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    addNote(argv.title, argv.body)
   }
 })
 
@@ -22,8 +37,15 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'remove a note',
-  handler: function () {
-    console.log('removing a note')
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    removeNote(argv.title)
   }
 })
 
@@ -46,5 +68,6 @@ yargs.command({
 })
 
 
-console.log(yargs.argv)
+// console.log(yargs.argv) or 
+yargs.parse()
 
